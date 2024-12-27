ded4d992df507681165f91274084f159151b6423
IvanaPedraza <ivanapedra234@gmail.com>
2024-12-27 18:38:25 -0500

    Desarrollo vista Home

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+              <a className="nav-link" href="/home">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/servicios">Servicios</a>
