bbf8d5ead8b55fab92060772cf806585accf2ad8
IvanaPedraza <ivanapedra234@gmail.com>
2024-12-28 00:34:01 -0500

    Primer avance Home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -12,8 +12,24 @@ const Home = () => {
             <Navbar />
 
             <div className="home-container">
-                <div className="home-content">
-                    
+                <div className="background-container">
+                
+                    <div className="home-content">
+                        <div className="left-text-container">
+                            ¡COMPRA <br /> LO MEJOR 
+                            <div className="subheading">
+                                Tenemos los mejores alimentos naturales y ecológicos en el país
+                            </div>
+                        </div>
+                        <h1 className="right-text-container">
+                            PARA TU <br /> MASCOTA!
+                        </h1>
+
+                        <div className="social-icons">
+                            <a href="#" className="social-icon instagram"></a>
+                            <a href="#" className="social-icon tiktok"></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
