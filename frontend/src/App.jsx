2d4de5f5c2914c977ca43b807111457357351217
Angel <apineross@unal.edu.co>
2024-12-28 13:12:04 -0500

    Remove duplicate imports in App.jsx (bugfix)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/login';
 import Register from './pages/register';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from './pages/login'
 import Home from './pages/home'
 
 function App() {
