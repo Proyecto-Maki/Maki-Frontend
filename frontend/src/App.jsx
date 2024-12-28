80d2c7fd93ac797546ac86ff18592fad91810d57
IvanaPedraza <ivanapedra234@gmail.com>
2024-12-28 17:21:03 -0500

    Ajuste constans y api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import react from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from './pages/login'
 import Home from './pages/home'
+import Register from './pages/register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
