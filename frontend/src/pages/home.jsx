849f55ba2f051611bd4be7e9208d60f48dd89840
IvanaPedraza <ivanapedra234@gmail.com>
2024-12-28 13:59:46 -0500

    Animación perro Home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../components/navbar';
 import '../styles/home.css';
 import logo from '../img/Logotipo Maki.png'; // Ruta al logo
+import cuerpo_Perro from '../img/cuerpo_Perro.png';
 
 const Home = () => {
 
@@ -12,22 +13,23 @@ const Home = () => {
             <Navbar />
 
             <div className="home-container">
-                <div className="backgroundHome-container">
-                    <div className="home-content">
-                        <div className="left-text-container">
-                            ¡COMPRA <br /> LO MEJOR 
-                            <div className="subheading">
-                                Tenemos los mejores alimentos naturales y ecológicos en el país
-                            </div>
+                <div className="dog__animation">
+                    <img src={cuerpo_Perro} alt="cuerpo_Perro" className="cuerpo_Perro" />
+                </div>
+                <div className="home-content">
+                    <div className="left-text-container">
+                        ¡COMPRA <br /> LO MEJOR 
+                        <div className="subheading">
+                            Tenemos los mejores alimentos naturales y ecológicos en el país
                         </div>
-                        <h1 className="right-text-container">
-                            PARA TU <br /> MASCOTA!
-                        </h1>
+                    </div>
+                    <h1 className="right-text-container">
+                        PARA TU <br /> MASCOTA!
+                    </h1>
 
-                        <div className="social-icons">
-                            <a href="#" className="social-icon instagram"></a>
-                            <a href="#" className="social-icon tiktok"></a>
-                        </div>
+                    <div className="social-icons">
+                        <a href="#" className="social-icon instagram"></a>
+                        <a href="#" className="social-icon tiktok"></a>
                     </div>
                 </div>
             </div>
