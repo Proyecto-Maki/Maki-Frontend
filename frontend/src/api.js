732b3d66d2cc09f2ef9d898feeadc9fdf6289031
Angel <apineross@unal.edu.co>
2024-12-28 13:28:36 -0500

    Update API URL to production and log the environment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
 const apiURL = 'https://maki-backend-production.up.railway.app/';
+console.log("API URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || apiURL,
 });
