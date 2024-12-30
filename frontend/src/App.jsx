e636d56646216a0dc7068420d998e1a85e059021
Angel <apineross@unal.edu.co>
2024-12-29 22:51:22 -0500

    Add Home component import to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/login';
 import Register from './pages/register';
 import ConfirmationRegister from './pages/confirmation-register';
+import Home from './pages/home';
 
 function App() {
   return (
