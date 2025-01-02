8e4815725b4ec552a5f2419704580563c67aeb9f
Angel <apineross@unal.edu.co>
2025-01-02 16:46:29 -0500

    Fix duplicate button in navbar component

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -33,8 +33,6 @@ function Navbar() {
         {/* Botón  para pantallas pequeñas */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon">☰</span>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon">☰</span>
         </button>
 
         {/* Links y botones */}
