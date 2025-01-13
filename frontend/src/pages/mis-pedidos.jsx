a1dfb822e30c8acb5d4faab4db3f5c520cb51e1e
kellysolanomt <ksolanoc@unal.edu.co>
2025-01-13 10:29:23 -0500

    Correccion de estilos de mis pedidos

--- frontend/src/pages/mis-pedidos.jsx
+++ frontend/src/pages/mis-pedidos.jsx
@@ -1,40 +1,47 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import "../styles/mis-pedidos.css";
+import { GiSittingDog } from "react-icons/gi";
 
 const Pedidos = () => {
-    const pedidos = [
-        { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
-        { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
-        { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
-        { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
-        { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
-        { id: "#2RYH7", total: 24000, fecha: "09/01/2025", estado: "Entregado" }
-    ];
+  const pedidos = [
+    { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+    { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+    { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+    { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+    { id: "#2RYH6", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+    { id: "#2RYH7", total: 24000, fecha: "09/01/2025", estado: "Entregado" },
+  ];
 
-    return (
-        <div className="absolute-pedidos-container">
-            <Navbar />
-            <div className="contenedor-pedidos">
-                {pedidos.map((pedido, index) => (
-                    <div key={index} className="tarjeta-pedido">
-                        <div className="fila-superior">
-                            <div className="icono"></div>
-                            <h4 className="id">{pedido.id}</h4>
-                            <i class="fa-solid fa-chevron-right" id="chevron"></i>
-                        </div>
-                        <div className="fila-intermedia">
-                            <p className="total">Total: ${pedido.total.toLocaleString()}</p>
-                            <button className="estado">{pedido.estado}</button>
-                        </div>
-                        <div className="fila-inferior">
-                            <p className="fecha">Fecha de compra: {pedido.fecha}</p>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="absolute-pedidos-container-mi">
+      <Navbar />
+      <div className="contenedor-pedidos-mi">
+        {pedidos.map((pedido, index) => (
+          <div key={index} className="tarjeta-pedido-mi">
+            <div className="fila-superior-mi">
+              <div className="icono-mi">
+                <GiSittingDog />
+              </div>
+              <div className="id-mi-container">
+                <h4 className="id-mi">{pedido.id}</h4>
+                <i class="fa-solid fa-chevron-right" id="chevron"></i>
+              </div>
             </div>
-        </div>
-    );
+            <div className="fila-intermedia-mi">
+              <p className="total-mi">
+                Total: ${pedido.total.toLocaleString()}
+              </p>
+              <button className="estado-mi">{pedido.estado}</button>
+            </div>
+            <div className="fila-inferior-mi">
+              <p className="fecha-mi">Fecha de compra: {pedido.fecha}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Pedidos;
