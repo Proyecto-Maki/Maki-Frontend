cebdcb1b1bd0d5b14fd0a19504e95664978f33bb
Angel <apineross@unal.edu.co>
2025-01-26 11:17:22 -0500

    Add payment handling and checkout component for Mercado Pago integration (Falta integrar en producción)

--- frontend/src/pages/carrito.jsx
+++ frontend/src/pages/carrito.jsx
@@ -37,6 +37,35 @@ const Carrito = () => {
     }
   };
 
+  const handlePayment = async () => {
+    try {
+      const items = cart.map((product) => ({
+        title: product.name,
+        quantity: product.quantity,
+        unit_price: product.price,
+        currency_id: "COP",
+      }));
+
+      console.log("Datos enviados al backend para crear la preferencia:", {
+        items,
+      });
+
+      // Llama al backend para crear la preferencia
+      const response = await api.post("/create_preference/", { items });
+
+      const initPoint = response.data.init_point; // Obtiene el init_point del backend
+      console.log("URL de Mercado Pago (init_point):", initPoint);
+
+      if (initPoint) {
+        window.location.href = initPoint; // Redirige al usuario al checkout de Mercado Pago
+      } else {
+        console.error("No se encontró init_point en la respuesta del backend.");
+      }
+    } catch (error) {
+      console.error("Error al iniciar el pago:", error);
+    }
+  };
+
   // Actualizar cantidad de un producto
   const updateProductQuantity = async (id, quantity) => {
     console.log(`Iniciando updateProductQuantity para producto ID ${id}...`);
@@ -244,7 +273,7 @@ const Carrito = () => {
                 <strong>TOTAL: ${totalPrice.toLocaleString()}</strong>
               </p>
               {/* Botón de pagar */}
-              <button className="Btn-carrito-pay">
+              <button className="Btn-carrito-pay" onClick={handlePayment}>
                 Pagar
                 <svg className="svgIcon" viewBox="0 0 576 512">
                   <path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"></path>
