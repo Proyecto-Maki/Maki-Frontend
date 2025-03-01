d2fcbb949b4a3de3442b89f47448155845ed86a9
IvanaPedraza <ivanapedra234@gmail.com>
2025-03-01 00:09:08 -0500

    adelanto mejoras diseño del Home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -12,6 +12,7 @@ import logo from "../img/Logotipo Maki.png";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { randomValue } from "../GenerateCardCode";
 import React, { useEffect, useState } from "react";
+import cat from "../img/gatoServicios.png";
 
 const Home = () => {
   const location = useLocation(); // Obtenemos la ubicación actual de la ruta
@@ -34,7 +35,7 @@ const Home = () => {
     <div className="absolute-home-container">
       {/* Navbar */}
 
-      <Parallax pages={2.2} className="animation-container">
+      <Parallax pages={3} className="animation-container">
         <Navbar />
         <ParallaxLayer offset={0} speed={1.8}>
           <div className="home-container">
@@ -82,32 +83,14 @@ const Home = () => {
             </div>
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0.999} speed={0.8} className="parallax-article">
+        <ParallaxLayer offset={0.99} speed={0.8} className="parallax-article">
           <div className="home-content-2">
             <div className="welcome-article-container">
               <div className="left-text-container-2">
                 <h2>
-                  <img
-                    src={paw}
-                    alt="paw"
-                    className="paw"
-                    style={{
-                      width: "50px",
-                      justifySelf: "center",
-                      paddingRight: "10px",
-                    }}
-                  />
+                  
                   ¡Bienvenid@ a Maki!{" "}
-                  <img
-                    src={paw}
-                    alt="paw"
-                    className="paw"
-                    style={{
-                      width: "50px",
-                      justifySelf: "center",
-                      paddingRight: "10px",
-                    }}
-                  />
+                  
                 </h2>
                 <p className="subheading-2">
                   En Maki, creemos que el bienestar de tu mascota y el cuidado
@@ -115,7 +98,7 @@ const Home = () => {
                   naturales libres de químicos, accesorios ecológicos, servicios
                   especializados de cuidado y fundaciones de la ciudad de
                   Bogotá, con las cuales podrás adoptar y/o donar.{" "}
-                  <p style={{ color: "#fcf3e3" }}>
+                  <p style={{ color: "#F4A258" }}>
                     {" "}
                     ¡Así podrás contribuir al bienestar animal!{" "}
                   </p>
@@ -129,11 +112,82 @@ const Home = () => {
               <div className="right-text-container-2">
                 <img src={maki} alt="maki" className="maki" />
               </div>
+              <div className="divider-footer-home">
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                  opacity=".25"
+                  className="shape-fill"
+                ></path>
+                <path
+                  d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                  opacity=".5"
+                  className="shape-fill"
+                ></path>
+                <path
+                  d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                  className="shape-fill"
+                ></path>
+              </svg>
+            </div>
+            </div>
+            <div class="custom-shape-divider-bottom-1736726305" >
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                  opacity=".25"
+                  class="shape-fill"
+                ></path>
+                <path
+                  d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                  opacity=".5"
+                  class="shape-fill"
+                ></path>
+                <path
+                  d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                  class="shape-fill"
+                ></path>
+              </svg>
             </div>
           </div>
+          <div className="Welcome-article-Services">
+            <h2 className="title-services">
+              Nuestros Servicios  
+            </h2>
+            <div className="container-cards">
+              <div className="card-donations">
+                <h2 className="donations-services">Donaciones</h2>
+                <div className="card-donations-content">
+                  <img 
+                    src={cat} 
+                    alt="cat" 
+                    className="cat_services" 
+                  />
+                  <p > Con Maki podrás realizar donaciones de tarjetas de regalo a las fundaciones inscritas en el apartado de fundaciones. 
+                    Con estas tarjetas podrán comprar productos únicamente en Maki para sus mascotas.
+                  </p>
+                </div>
+                
+              </div>
+              <div className="card-adoptions">
+
+              </div>
+            </div>
+          </div>
+          
         </ParallaxLayer>
         <ParallaxLayer
-          offset={1.4}
+          offset={2}
           speed={1}
           className="footer-container-home-parallax"
           style={{
@@ -206,7 +260,7 @@ const Home = () => {
                   </li>
                 </ul>
               </div>
-              <hr className="footer-divider-home" /> {/*linea divisoria*/}
+              <hr className="footer-divider-home" />
               <div className="footer-contact-home">
                 <div className="footer-social-home">
                   <div className="social-icon">
